77f13e19578505f1f792140d6bf4dceacfbc7558
John Wolf <20865568+jwolf842@users.noreply.github.com>
2025-10-07 09:53:08 -0400

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Installing Bicycle Gear Clock...
+echo.
+echo Opening Lively Wallpaper download page...
+start https://www.rocksdanister.com/lively/
+echo.
+echo Preview the clock in your browser:
+pause
+start bicycle-gear-clock.html
+echo.
+echo Once Lively is installed, add bicycle-gear-clock.html as wallpaper
+pause
